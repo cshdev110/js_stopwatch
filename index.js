214bc2db5d9a1f807833fab869f6ff57f953cf04
Dan <dan.devars8@gmail.com>
2023-01-26 10:58:37 +1000

    Start, pause, reset. Time format.

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
  */
 
 /*testing purpose*/
+/*
 const dateNowl = document.querySelector("#dateNowl");
 const dateNowInter = setInterval(date_now, 1000);
 function date_now(){
     dateNowl.innerHTML = Date.now();
-    // clearInterval(dateNowInter);
+    //// clearInterval(dateNowInter);
 }
-/**/
-
+*/
 
 const timeDisplay = document.querySelector("#timeDisplay");
 const startBtn = document.querySelector("#startBtn");
@@ -37,8 +37,40 @@ startBtn.addEventListener("click", () => {
         intervalId = setInterval(updateTime, 1000);
     }
 });
-pauseBtn.addEventListener("click", () => {});
-resetBtn.addEventListener("click", () => {});
+pauseBtn.addEventListener("click", () => {
+    if(!paused){
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+});
+resetBtn.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00:00:00";
+});
 
 function updateTime(){
+    elapsedTime = Date.now() - startTime;
+
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+    function pad(unit){
+        //The addition of ("") is to become unit to string so length can work.
+        return (unit + "").length == 1 ? "0" + unit : unit;
+    }
 }
